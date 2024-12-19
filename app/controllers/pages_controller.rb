62d50baea53770b4cee15b133af3e1542fa55c85
Christian Sanjo <christianvelez193@hotmail.com>
2024-12-19 17:23:09 +0900

    added columns and can make tweets

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,22 @@
 class PagesController < ApplicationController
 
   def home
+    @tweet = Tweet.new
+    @user = current_user
+    if current_user.nil?
+      @tweets = Tweet.all.order("created_at DESC")
+    else
+      @tweets = []
+      current_user.followers.each do |follower|
+        follower.tweets.each do |tweet|
+          @tweets << tweet
+        end
+      end
+      @user.tweets.each do |tweet|
+        @tweets << tweet
+      end
+      @tweets = @tweets.sort_by { |tweet| tweet.created_at}.reverse
+    end
   end
 
 end
--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,2 +1,19 @@
 class TweetsController < ApplicationController
+
+  def create
+    @tweet = Tweet.new(tweet_params)
+    @tweet.user = current_user
+    if @tweet.save
+      redirect_to root_path
+    else
+      redirect_to root_path
+    end
+  end
+
+
+  private
+
+  def tweet_params
+    params.require(:tweet).permit(:description)
+  end
 end
