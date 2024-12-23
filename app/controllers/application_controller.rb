eae7181e80f29d64d2ee4fda087feb9f95a678cd
Christian Sanjo <christianvelez193@hotmail.com>
2024-12-23 15:46:34 +0900

    fixing cloudinary upload

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,7 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username, { photos: [] }])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:username, { photos: [] }])
   end
 end
